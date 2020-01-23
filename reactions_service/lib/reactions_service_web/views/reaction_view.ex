f406540091e6ec0d591668940cbfb71e2b6b5015
Robley <robleyadrian@gmail.com>
2020-01-23 19:06:16 +0300

    add view to render response messages and reaction counts

--- reactions_service/lib/reactions_service_web/views/reaction_view.ex
+++ reactions_service/lib/reactions_service_web/views/reaction_view.ex
@@ -18,4 +18,15 @@ defmodule ReactionsServiceWeb.ReactionView do
       user_id: reaction.user_id,
       reaction_type: reaction.reaction_type}
   end
+
+  def render("reactions_count.json", counts_data) do
+    %{
+      content_id: counts_data.content_id,
+      reaction_count: counts_data.counts
+    }
+  end
+
+  def render("response.json", %{response: response}) do
+    %{msg: response}
+  end
 end
